0f2ceb6717b403e7aecfda2d369f063969852c91
Ana Pauta <alpauta73@gmail.com>
2022-12-13 12:06:57 +0100

    Intento 1

---
+++ src/main/java/myFirstClass.java
@@ -0,0 +1,22 @@
+public class myFirstClass {
+    public static void  main(String[] args) {
+        System.out.println(suma(1,2,3));
+
+        coche miCoche = new coche();
+        miCoche.increaseNumDoors();
+        System.out.println(miCoche.numDoors);
+    }
+
+    public static int suma(int a , int b, int c){
+        return a + b + c;
+    }
+}
+
+class coche{
+    public int numDoors =0;
+
+    public void increaseNumDoors(){
+        this.numDoors++;
+    }
+
+}
